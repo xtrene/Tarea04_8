ee8dc19bc011a80584af04f48ecd0df38821ed33
Carlos <Carlos@xtrene.com>
2021-06-21 19:44:39 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tarea04_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Los primeros 100 números: ");
+            while (num < 100)
+            {
+                num++;
+                Console.Write(" " + num);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
